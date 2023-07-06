6edcdaacfc1e50f53b2cdeab816941b45dea55ca
Tupalski Ilya <itupalski@gmail.com>
2023-07-06 11:28:52 +0400

    feat: start project php-site (Thu Jul  6 11:28:52 +04 2023)

--- error.php
+++ error.php
@@ -5,11 +5,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="https://img.icons8.com/ios-glyphs/30/228BE6/php.png" type="image/x-icon">
-  <title>Blog</title>
+  <title>Error</title>
 </head>
 
 <body>
-
+  <p>Error 404!</p>
 </body>
 
 </html>
