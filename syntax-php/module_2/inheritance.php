4872b49663da130b3a4fd1ae4f0b43a61dd300af
Tupalski Ilya <itupalski@gmail.com>
2023-07-11 14:21:55 +0400

    feat: add lesson inheritance (Tue Jul 11 14:21:55 +04 2023)

---
+++ syntax-php/module_2/inheritance.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP</title>
+</head>
+<body>
+  <?php
+  class Car {
+    public $speed;
+    protected $wheels;
+    protected $color;
+
+    function __construct($speed, $color) {
+      $this->speed = $speed;
+      $this->color = $color;
+    }
+
+    function showSpeed() {
+      echo "Speed car: " . $this->speed . '<br>';
+    }
+  }
+
+  class BMW extends Car {
+    private $model;
+
+    function __construct($speed, $color, $model) {
+      parent::__construct($speed, $color);
+      $this->model = $model;
+    }
+    
+    function setModel() {
+      echo "Model car: " . $this->model . '<br>';
+      echo "Color car: " . $this->color . '<br>';
+    }
+  }
+
+  class Audi extends Car {}
+
+  $m3 = new BMW(340, "black", "m3");
+  $m3->showSpeed();
+  $m3->setModel();
+  ?>
+</body>
+</html>
