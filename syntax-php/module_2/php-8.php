5ee680312f3d5b6655a3742ec55e559b588a0cb5
Tupalski Ilya <itupalski@gmail.com>
2023-07-12 13:33:17 +0400

    feat: add lesson php 8 (Wed Jul 12 13:33:17 +04 2023)

---
+++ syntax-php/module_2/php-8.php
@@ -0,0 +1,46 @@
+<?php
+echo "<h2>PHP 8</h2>";
+
+// class Point {
+//   public $x;
+//   public $y;
+//   public $z;
+
+//   public function __construct($x, $y, $z) {
+//     $this->x = $x;
+//     $this->y = $y;
+//     $this->z = $z;
+//   }
+// }
+
+class Point
+{
+  public function __construct(public int|float $x = 0.0, public int|float $y = 0.0, public int|float $z = 0.0)
+  {
+    $this->x = $x;
+    $this->y = $y;
+    $this->z = $z;
+  }
+}
+
+$zero = new Point(x: 4, z: 5, y: 8);
+echo "Coordinat: " . $zero->x . ", " . $zero->y . ", " . $zero->z . '<br>';
+
+$num = 9.34;
+// switch ($num) {
+//   case 5:
+//     $result = "number";
+//     break;
+//   case "5":
+//     $result = 'not a number';
+//     break;
+// }
+
+// echo $result;
+
+echo match($num) {
+  '9.34' => "not a number",
+  9.34 => "number",
+};
+
+$country = $session?->user?->good;
